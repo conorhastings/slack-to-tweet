ccd55e82b18978b35bcb1cc2656ec2ace7c285a1
conorhastings <hastings.conorm@gmail.com>
2015-09-27 11:35:47 -0400

    delete server.js from this module

--- server.js
+++
@@ -1,8 +0,0 @@
-var slackToTweet = require("./index.js");
-var slackToTweetSettings = {
-	twitterConsumerKey: process.env.CONSUMER_KEY,
-	twitterConsumerSecret: process.env.CONSUMER_SECRET,
-	twitterAccessTokenKey: process.env.ACCESS_TOKEN,
-	twitterAccessTokenSecret: process.env.ACCESS_TOKEN_SECRET
-};
-var slackToTweetServer = slackToTweet(slackToTweetSettings).listen(process.env.PORT || 4000);
